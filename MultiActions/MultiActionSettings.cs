259a3c5fe2a1af433989f9c87125f7c9be41ec7a
Tolfx <matteus@tolfx.dev>
2022-06-07 20:20:14 +0200

    feat: :package: Updated to 1.0.2

--- MultiActions/MultiActionSettings.cs
+++ MultiActions/MultiActionSettings.cs
@@ -5,7 +5,7 @@ namespace MultiActions
     internal static class MultiActionSettings
     {
         internal static string ModName = "MultiActions";
-        internal static string ModVersion = "1.0.1";
+        internal static string ModVersion = "1.0.2";
         internal static MelonPreferences_Entry<bool> enable;
         internal static MelonPreferences_Entry<bool> quitButton;
         internal static MelonPreferences_Entry<bool> respawnButton;
