022e30e093d2c60341e733f7bfbc3e4cd6926098
Tolfx <matteus@tolfx.dev>
2022-06-08 18:18:16 +0200

    feat: :label: Updated version

--- MultiActions/Properties/AssemblyInfo.cs
+++ MultiActions/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using MelonLoader;
 using MultiActions;
 
-[assembly: MelonInfo(typeof(MultiActionsMod), "MultiActions", "1.0.4", "Tolfx", "https://github.com/Tolfx/MultiActions/releases/latest/download/MultiActions.dll")]
+[assembly: MelonInfo(typeof(MultiActionsMod), "MultiActions", "1.0.5", "Tolfx", "https://github.com/Tolfx/MultiActions/releases/latest/download/MultiActions.dll")]
 [assembly: MelonGame("VRChat", "VRChat")]
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
