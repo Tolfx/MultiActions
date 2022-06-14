8d01d7bfdbcdc0864c58eb3365d736e11428703a
Tolfx <matteus@tolfx.dev>
2022-06-14 10:28:55 +0200

    feat: :sparkles: Moved actions to seperate file

--- MultiActions/ActionMenu/Actions.render.cs
+++ MultiActions/ActionMenu/Actions.render.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using MelonLoader;
 using ActionMenuApi.Api;
+using ActionMenuApi.Helpers;
+using ActionMenuApi.Managers;
 using VRC.UserCamera;
 using VRC.SDKBase;
 using VRC.Core;
@@ -28,10 +30,10 @@ namespace MultiActions.ActionMenu
 
         public static void Init()
         {
-            MultiActions.ActionMenu.Actions.MaActionsMain.Init();
-            MultiActions.ActionMenu.Actions.MaActionsOpen.Init();
-            MultiActions.ActionMenu.Actions.MaActionTeleport.Init();
-            MultiActions.ActionMenu.Actions.MaActionsTeleporting.Init();
+            // MultiActions.ActionMenu.Actions.MaActionsMain.Init();
+            // MultiActions.ActionMenu.Actions.MaActionsOpen.Init();
+            // MultiActions.ActionMenu.Actions.MaActionTeleport.Init();
+            // MultiActions.ActionMenu.Actions.MaActionsTeleporting.Init();
         }
 
         public static void Render()
@@ -40,37 +42,189 @@ namespace MultiActions.ActionMenu
             if (!MultiActionSettings.IsModEnabled())
                 return;
 
-            // Let's create our "categories"
             VRCActionMenuPage.AddSubMenu(
                 ActionMenuPage.Main, 
                 MultiActionSettings.ModName,
                 () =>
                 {
+                    /// <summary>
+                    /// Taking out/in camera button
+                    /// </summary>
+                    CustomSubMenu.AddButton(
+                        Utils.Camera.isCameraOn ? "Take in camera" : "Take out camera",
+                        Utils.Camera.CameraToggle, 
+                        null,
+                        !MultiActionSettings.IsModEnabled()
+                    );
 
-                    // Let's render our "Main" actions
-                    CustomSubMenu.AddSubMenu("Main", () =>
-                    {
-                        RenderMainActions();
-                    });
+                    /// <summary>
+                    /// Respawns our local player
+                    /// </summary>
+                    CustomSubMenu.AddButton(
+                        "Respawn", 
+                        Utils.Players.respawnLocalPlayer, 
+                        null, 
+                        (!MultiActionSettings.IsModEnabled() || !MultiActionSettings.respawnButton.Value)
+                    );
 
+                    /// <summary>
+                    /// Quits game
+                    /// </summary>
+                    CustomSubMenu.AddSubMenu(
+                        "Quit game", 
+                        () =>
+                        {
+                            CustomSubMenu.AddButton(
+                                "Yes",
+                                UnityEngine.Application.Quit,
+                                null,
+                                !MultiActionSettings.IsModEnabled()
+                            );
+                        }, null,
+                        (!MultiActionSettings.IsModEnabled() || !MultiActionSettings.quitButton.Value)
+                    );
+
+                    /// <summary>
+                    /// Opens different menus for local player
+                    /// </summary>
                     CustomSubMenu.AddSubMenu("Open", () => 
                     {
-                        RenderOpenActions();
-                    });
+                        CustomSubMenu.AddButton("Avatars", () =>
+                        {
+                            VRCUiManagerEx.Instance.ShowUi();
+                            VRCUiManagerEx.Instance.ShowScreen(QuickMenu.MainMenuScreenIndex.AvatarMenu);
+                        }, null, !MultiActionSettings.IsModEnabled());
 
-                    CustomSubMenu.AddSubMenu("Teleport to", () =>
+                        CustomSubMenu.AddButton("Settings", () =>
+                        {
+                            VRCUiManagerEx.Instance.ShowUi();
+                            VRCUiManagerEx.Instance.ShowScreen(QuickMenu.MainMenuScreenIndex.SettingsMenu);
+                        }, null, !MultiActionSettings.IsModEnabled());
+
+                        CustomSubMenu.AddButton("Safety", () =>
+                        {
+                            VRCUiManagerEx.Instance.ShowUi();
+                            VRCUiManagerEx.Instance.ShowScreen(QuickMenu.MainMenuScreenIndex.SafetyMenu);
+                        }, null, !MultiActionSettings.IsModEnabled());
+                    }, 
+                    null,
+                    // When using this custom submenu and we open a setting
+                    // We the user who is in XRDevice will be stuck
+                    // So just ensuring we are not in XRDevice
+                    (!MultiActionSettings.IsModEnabled() || !Utils.Extra.isInXR()));
+
+                    // To the guidelines of VRCMG you need to check allowed or not.
+                    // Lets simply not render if we don't want risky functions enabled
+                    if (MultiActionSettings.riskyF.Value && MultiActionSettings.allowedForRisky())
                     {
-                        RenderTeleportActions();
-                    });
 
-                    CustomSubMenu.AddSubMenu("Teleporting", () =>
-                    {
-                        RenderTeleportingActions();
-                    });
+                        /// <summary>
+                        /// Teleports to player
+                        /// </summary>
+                        CustomSubMenu.AddSubMenu("Teleport to", () =>
+                        {
+                            var allPlayers = Utils.Players.getAllPlayers();
+                            foreach (var player in allPlayers)
+                            {
+                                CustomSubMenu.AddSubMenu(
+                                    player.field_Private_APIUser_0.displayName,
+                                    () =>
+                                    {
+                                        CustomSubMenu.AddButton(
+                                            $"TP to: {player.field_Private_APIUser_0.displayName}",
+                                            () => TeleportHandler.TeleportTo(player),
+                                            null, 
+                                            !MultiActionSettings.IsModEnabled()
+                                        );
+                                    },
+                                    null,
+                                    !MultiActionSettings.IsModEnabled()
+                                );
+                            }
+                        }, null, !MultiActionSettings.IsModEnabled());
 
-                }, null
-            );
+                        /// <summary>
+                        /// Teleporting menu
+                        /// </summary>
+                        CustomSubMenu.AddSubMenu("Teleporting", () =>
+                        {
 
+                            CustomSubMenu.AddButton("Save position", () =>
+                            {
+                                var player = Utils.Players.getLocalPlayer();
+                                if (player == null) return;
+
+                                var controller = VRCPlayer.field_Internal_Static_VRCPlayer_0.GetComponent<GamelikeInputController>();
+                                controller.enabled = false;
+                                BuiltinUiUtils.ShowInputPopup(
+                                    "Save Point Name", 
+                                    "", 
+                                    InputField.InputType.Standard, 
+                                    false, 
+                                    "Save", 
+                                    (msg, _, _2) =>
+                                    {
+                                        controller.enabled = true;
+                                        TeleportHandler.AddSavePoint(msg, player.transform.position);
+                                        var nButtonSaved = ReMenuButton.Create(
+                                            msg,
+                                            "Click to teleport",
+                                            () =>
+                                            {
+                                                TeleportHandler.TeleportTo(TeleportHandler.GetSavePoint(msg));
+                                            },
+                                            MultiActionsMod.TeleportsCategory.RectTransform
+                                        );
+                                        MultiActionsMod.SavedPointsButtons.Add(msg, nButtonSaved);
+                                    },
+                                    () => controller.enabled = true
+                                );
+                            }, null, !MultiActionSettings.IsModEnabled());
+
+                            CustomSubMenu.AddSubMenu("Saved", () =>
+                            {
+                                CustomSubMenu.AddButton("Clear all", () =>
+                                {
+                                    TeleportHandler.ClearAllSavePoints();
+                                    // Loop through all buttons and Destroy them
+                                    foreach (var button in MultiActionsMod.SavedPointsButtons)
+                                    {
+                                        button.Value.Destroy();
+                                    }
+                                    MultiActionsMod.SavedPointsButtons.Clear();
+                                }, null, !MultiActionSettings.IsModEnabled());
+
+                                // Display all of our saves
+                                var points = TeleportHandler.GetSavePoints();
+                                foreach(var point in points)
+                                {
+                                    var p = TeleportHandler.GetSavePoint(point);
+                                    if (p == null) return;
+                                    CustomSubMenu.AddSubMenu(point, () =>
+                                    {
+                                        CustomSubMenu.AddButton("Delete", () =>
+                                        {
+                                            TeleportHandler.RemoveSavePoint(point);
+                                            var temp = MultiActionsMod.SavedPointsButtons[point];
+                                            MultiActionsMod.SavedPointsButtons.Remove(point);
+                                            temp.Destroy();
+                                        });
+                                        CustomSubMenu.AddButton("Teleport", () =>
+                                        {
+                                            // Teleport player to p
+                                            var player = Utils.Players.getLocalPlayer();
+                                            if (player == null) return;
+                                            TeleportHandler.TeleportTo(p);
+                                        }, null, !MultiActionSettings.IsModEnabled());
+                                    });
+                                }
+                            });
+
+                        }, null, !MultiActionSettings.IsModEnabled());
+
+                    }
+                    
+                }, null, !MultiActionSettings.IsModEnabled());
         }
 
         public static void AddAction(MaAction action)
@@ -78,53 +232,93 @@ namespace MultiActions.ActionMenu
             MaActionsList.Add(action);
         }
 
-        private static void RenderMainActions()
+        private static void RenderActions(MaHigherActions pos)
         {
             // Lets get all MaActionsList with the "Main" category
-            var mainActions = MaActionsList.Where(x => x.HigherAction == MaHigherActions.Main).ToList();
+            var actions = MaActionsList.Where(x => x.HigherAction == pos).ToList();
             // Let's loop through all of our actions
-            foreach (var action in mainActions)
+            foreach (var action in actions)
             {
-                // Let's add the button
+                // Check if subAction
+                if (action.hasSubMenu)
+                {
+                    // Add submenu
+                    CustomSubMenu.AddSubMenu(action.Name, () =>
+                    {
+                        RenderActions(pos, action.subAction);
+                    });
+                }
+                else if (action.hasSubMenus)
+                {
+                    // Add submenu
+                    CustomSubMenu.AddSubMenu(action.Name, () =>
+                    {
+                        RenderActions(pos, action.subActions);
+                    });
+                }
+                else
+                {
+                    // Add button
+                    action.AddButton();
+                }
+            }
+        }
+
+        private static void RenderActions(MaHigherActions pos, MaAction action)
+        {
+            // Check if subAction
+            if (action.hasSubMenu)
+            {
+                // Add submenu
+                CustomSubMenu.AddSubMenu(action.Name, () =>
+                {
+                    RenderActions(pos, action.subAction);
+                });
+            }
+            else if (action.hasSubMenus)
+            {
+                // Add submenu
+                CustomSubMenu.AddSubMenu(action.Name, () =>
+                {
+                    RenderActions(pos, action.subActions);
+                });
+            }
+            else
+            {
+                // Add button
                 action.AddButton();
             }
         }
 
-        private static void RenderTeleportingActions()
+        private static void RenderActions(MaHigherActions pos, MaAction[] actions)
         {
-            // Lets get all MaActionsList with the "Teleporting" category
-            var teleportingActions = MaActionsList.Where(x => x.HigherAction == MaHigherActions.Teleporting).ToList();
-            // Let's loop through all of our actions
-            foreach (var action in teleportingActions)
+            foreach(var action in actions)
             {
-                // Let's add the button
-                action.AddButton();
+                // Check if subAction
+                if (action.hasSubMenu)
+                {
+                    // Add submenu
+                    CustomSubMenu.AddSubMenu(action.Name, () =>
+                    {
+                        RenderActions(pos, action.subAction);
+                    });
+                } 
+                else if (action.hasSubMenus)
+                {
+                    // Add submenu
+                    CustomSubMenu.AddSubMenu(action.Name, () =>
+                    {
+                        RenderActions(pos, action.subActions);
+                    });
+                }
+                else
+                {
+                    // Add button
+                    action.AddButton();
+                }
             }
         }
 
-        private static void RenderTeleportActions()
-        {
-            // Lets get all MaActionsList with the "Teleport" category
-            var teleportActions = MaActionsList.Where(x => x.HigherAction == MaHigherActions.Teleport).ToList();
-            // Let's loop through all of our actions
-            foreach (var action in teleportActions)
-            {
-                // Let's add the button
-                action.AddButton();
-            }
-        }
-
-        private static void RenderOpenActions()
-        {
-            // Lets get all MaActionsList with the "Open" category
-            var openActions = MaActionsList.Where(x => x.HigherAction == MaHigherActions.Open).ToList();
-            // Let's loop through all of our actions
-            foreach (var action in openActions)
-            {
-                // Let's add the button
-                action.AddButton();
-            }
-        }
     }
 
 }
